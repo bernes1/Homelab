386ed7b457826e2d5ff79d7c034703afc4366291
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-07-26 03:00:45 +0200

    Add Terraform configuration for Proxmox provider and Docker services

---
+++ home-prod/terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1-rc1"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://proxmox-server01.example.com:8006/api2/json"
+}
