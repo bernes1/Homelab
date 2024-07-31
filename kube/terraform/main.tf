97c65438018b39c94b1a19760def86aeeca98d52
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-07-31 18:48:20 +0200

    feat: Add Terraform configuration for Proxmox VMs and Docker services

--- kube/terraform/main.tf
+++ kube/terraform/main.tf
@@ -1,7 +1,9 @@
-resource "proxmox_vm_qemu" "talos-ctp" {
-  name        = "talos-ctp01"
+resource "proxmox_vm_qemu" "talos-cp" {
+  count       = 3
+  name        = "talos-cp0${count.index}"
   target_node = "pve01"
   clone       = "talos-template"
+  agent       = 1
 
   disks {
     scsi {
@@ -25,4 +27,31 @@ resource "proxmox_vm_qemu" "talos-ctp" {
   memory = 4096
 }
 
+resource "proxmox_vm_qemu" "talos-node" {
+  count       = 3
+  name        = "talos-node0${count.index}"
+  target_node = "pve01"
+  clone       = "talos-template"
+  agent       = 1
 
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage    = "local-lvm"
+          size       = "32G"
+          emulatessd = true
+        }
+      }
+    }
+    ide {
+      ide2 {
+        cdrom {
+          iso = "local:iso/nocloud-amd64.iso"
+        }
+      }
+    }
+  }
+  cores  = 2
+  memory = 4096
+}
